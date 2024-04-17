32fc9d35158e0e64fa3f376cff509951e952cba1
mimsy02 <miyang27@amherst.edu>
2024-04-17 17:00:28 -0400

    add SleepHistory, SleepNode, and packages

--- SleepTracker.java
+++ SleepTracker.java
@@ -1,11 +1,37 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.util.Random;
+import java.awt.Graphics2D;
+
 public class SleepTracker{
 
 }
 
 class User{
-    //thuaeihtjsirhgjsfjhsbgbdsfjhbvhjsdfbvb
+
 }
 
 class logSleep {
-    // dkfkdfkdljfldjf hihihi
+
+}
+
+class SleepHistory{
+    private int averageSleepDuration;
+
+    public SleepHistory(){
+    }
+    // contains history of sleep from past week
+    /* have a wrapper - something that contains
+     * sleep data per day that week*/
+    // calculate average sleep time method
+    // delete old sleep data method
+    // draw method
+    // sleep recommendations method??
+}
+
+class SleepNode{
+    // sleep data for a single day
 }
